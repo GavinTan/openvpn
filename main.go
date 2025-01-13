640f8a5674d7258d1d4591240e78c75cd4f60244
tw <987146971@qq.com>
2025-01-13 17:26:17 +0800

    fix: 修改web密码后调用ovpn login接口错误

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/exec"
-	"strings"
 	"time"
 )
 
@@ -20,74 +18,17 @@ func init() {
 }
 
 func main() {
-	var adminUser, adminPass string
-
 	username := os.Getenv("username")
 	password := os.Getenv("password")
-	auth_token := os.Getenv("auth_token")
 
-	opvn_data, ok := os.LookupEnv("ovpn_data")
-	if !ok {
-		opvn_data = "/data"
-	}
-
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("source %s/.vars && echo '%s' | openssl enc -d -aes-256-cbc -a -pbkdf2 -k $SECRET_KEY", opvn_data, auth_token))
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		if out == nil {
-			out = []byte(err.Error())
-		}
-		log.Println(strings.TrimSpace(string(out)))
-		os.Exit(1)
-	} else {
-		if out != nil {
-			d := strings.Split(strings.TrimSpace(string(out)), ":")
-			if len(d) == 2 {
-				adminUser = d[0]
-				adminPass = d[1]
-			}
-		}
-	}
-
-	lresp, err := http.PostForm(os.Getenv("auth_api"), url.Values{"username": {adminUser}, "password": {adminPass}})
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	defer lresp.Body.Close()
-	io.Copy(io.Discard, lresp.Body)
-
-	if lresp.StatusCode != 200 {
-		log.Println("Login failed")
-		os.Exit(1)
-	}
-	cookie := lresp.Header.Get("Set-Cookie")
-
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	b := url.Values{}
-	b.Add("username", username)
-	b.Add("password", password)
-
-	req, err := http.NewRequest("POST", os.Getenv("ovpn_auth_api"), strings.NewReader(b.Encode()))
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookie", cookie)
-
-	resp, err := client.Do(req)
+	resp, err := http.PostForm(os.Getenv("ovpn_auth_api"), url.Values{"username": {username}, "password": {password}})
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
