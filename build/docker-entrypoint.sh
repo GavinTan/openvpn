0083917410f74b46fcbf324373e2a7327b41b259
tw <987146971@qq.com>
2025-07-31 10:09:25 +0800

    perf: 优化自动获取出口IP方式

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -265,7 +265,7 @@ genclient() {
     cat << EOF > $OVPN_DATA/clients/$1.ovpn
 client
 proto $([[ "$OVPN_IPV6" == "true" ]] && [[ ! "$OVPN_PROTO" =~ 6 ]] && echo "${OVPN_PROTO}6" || echo $OVPN_PROTO)
-remote ${2:-$([[ "$OVPN_IPV6" == "true" ]] && ip -6 route get 2001:4860:4860::8888 | awk {'print $7'} | tr -d '\n' || ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n')} $OVPN_PORT
+remote ${2:-$([[ "$OVPN_IPV6" == "true" ]] && ip -6 route get 2001:4860:4860::8888 | grep -oP 'src \K\S+' || ip -4 route get 8.8.8.8 | grep -oP 'src \K\S+')} $OVPN_PORT
 dev tun
 resolv-retry infinite
 nobind
